269cf29ac89e35e95a2a14392710bf5acba7279a
Kedari Chowtoori <53082047+ChowtooriKedari@users.noreply.github.com>
2025-04-07 21:15:17 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <div className="w-full py-2">
         <p className="text-center text-gray-500 text-base">
-          © 2025. Built by Kedari Chowtoori
+          © 2025 Built by Kedari Chowtoori
           <br />
         </p>
       </div>

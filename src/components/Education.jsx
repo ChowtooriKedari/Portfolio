daacd722de2641d518d008a05d5904e7e634044b
Kedari Chowtoori <53082047+ChowtooriKedari@users.noreply.github.com>
2025-04-07 21:14:19 -0400

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,7 +8,10 @@ import ViswamIcon from '../assets/Viswam.png';
 const educationData = [
   {
     id: 1,
-    degree: "Masters in Computer Science - Digital Arts",
+    degree: [
+      " Masters in Computer Science - Digital Arts",
+      " Research assistant in RISC Lab "
+      ],
     institution: "Dartmouth College, Hanover, NH, USA",
     duration: "2023-2025",
     details: "",

5d984ef3a16bca27dc335e3c84819a751233998d
Kedari Chowtoori <53082047+ChowtooriKedari@users.noreply.github.com>
2025-07-12 12:23:35 -0400

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -204,13 +204,6 @@ export const TangibleUIData=[
     id:7,
     title:"Arduino and LEDs",
     videoLink: "https://youtu.be/WVHvqrBwxjQ"
-  },
-    {
-    id:8,
-    title: "Interactive Yoga",
-    des: "Developed an immersive yoga experience that combines real-time pose detection, breathing feedback, and dynamic fluid art using Arduino, MediaPipe, and Unity.",
-    attachmentLink: "https://drive.google.com/file/d/1UHE8LgTVZwIMsgS7v48KYdwej9pOx2W0/view?usp=sharing",
-    videoLink: "https://youtu.be/WVHvqrBwxjQ"
   }
 ];
 export const workExperiences= [
